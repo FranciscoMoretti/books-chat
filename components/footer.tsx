6d898e8e11faa3f1d47aeb8ff714220f4a9eb526
Francisco Moretti <franciscoemoretti@gmail.com>
2024-05-17 19:11:01 +0100

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,19 +5,24 @@ import { ExternalLink } from '@/components/external-link'
 
 export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
   return (
-    <p
-      className={cn(
-        'px-2 text-center text-xs leading-normal text-muted-foreground',
-        className
-      )}
-      {...props}
-    >
-      Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
-      </ExternalLink>
-      .
-    </p>
+    <footer className="border-t border-t-secondary w-full">
+      <div className="flex container flex-col items-center justify-center gap-2 md:flex-row py-4  ">
+        <div className="flex flex-col items-center md:flex-row gap-2 ">
+          <p className="px-2 text-center text-xs leading-normal text-muted-foreground">
+            Built by{' '}
+            <ExternalLink href={'https://twitter.com/franmoretti_'}>
+              franmoretti_
+            </ExternalLink>
+            . Open source on{' '}
+            <ExternalLink
+              href={'https://github.com/FranciscoMoretti/books-chat'}
+            >
+              GitHub
+            </ExternalLink>
+            .
+          </p>
+        </div>
+      </div>
+    </footer>
   )
 }
