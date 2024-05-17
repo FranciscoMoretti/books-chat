d384d9f9ca79b2c0b3671e5637250c9202079b37
Francisco Moretti <franciscoemoretti@gmail.com>
2024-05-17 17:55:51 +0100

    github on the right

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import { IconGitHub } from '@/components/ui/icons'
 export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-      <div className="flex items-center justify-end space-x-2">
+      <div className="w-full flex items-center justify-end space-x-2">
         <a
           target="_blank"
           href="https://github.com/FranciscoMoretti/books-chat/"
